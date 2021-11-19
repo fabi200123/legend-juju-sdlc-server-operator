84bc7a0a1ac15603f8f92efb317942839c835423
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2021-11-19 12:03:33 +0200

    Linting and testing compliance patches. (#5)
    * Bump all required charm library versions.
    
    * Linting and formatting compliance changes.
    
    * Add linting/unit testing GitHub CI hooks.

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -1,9 +1,9 @@
-# Copyright 2021 Canonical
+# Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
-#
-# Learn more about testing at: https://juju.is/docs/sdk/testing
 
-from unittest import mock
+"""Module testing the Legend SDLC Operator."""
+
+import json
 
 from charms.finos_legend_libs.v0 import legend_operator_testing
 from ops import testing as ops_testing
@@ -11,22 +11,37 @@ from ops import testing as ops_testing
 import charm
 
 
-class LegendSdlcTestWrapper(
-    charm.LegendSDLCServerCharm, legend_operator_testing.BaseFinosLegendCoreServiceTestCharm
-):
+class LegendSdlcTestWrapper(charm.LegendSDLCServerCharm):
+    @classmethod
+    def _get_relations_test_data(cls):
+        return {
+            cls._get_legend_db_relation_name(): {
+                "legend-db-connection": json.dumps(
+                    {
+                        "username": "test_db_user",
+                        "password": "test_db_pass",
+                        "database": "test_db_name",
+                        "uri": "test_db_uri",
+                    }
+                )
+            },
+            cls._get_legend_gitlab_relation_name(): {
+                "legend-gitlab-connection": json.dumps(
+                    {
+                        "gitlab_host": "gitlab_test_host",
+                        "gitlab_port": 7667,
+                        "gitlab_scheme": "https",
+                        "client_id": "test_client_id",
+                        "client_secret": "test_client_secret",
+                        "openid_discovery_url": "test_discovery_url",
+                        "gitlab_host_cert_b64": "test_gitlab_cert",
+                    }
+                )
+            },
+        }
 
-    WORKLOAD_CONTAINER_NAME = "sdlc"
-    WORKLOAD_SERVICE_NAMES = ["sdlc"]
-    DB_RELATION_NAME = "legend-db"
-    GITLAB_RELATION_NAME = "legend-sdlc-gitlab"
-    RELATIONS = [DB_RELATION_NAME, GITLAB_RELATION_NAME]
-    RELATIONS_DATA = {
-        DB_RELATION_NAME: {"database": "DB relation test data"},
-        GITLAB_RELATION_NAME: {"gitlab": "GitLab relation test data"},
-    }
-
-    def _get_jks_truststore_preferences(self):
-        return self.TRUSTSTORE_PREFERENCES
+    def _get_service_configs_clone(self, relation_data):
+        return {}
 
 
 class LegendSdlcTestCase(legend_operator_testing.TestBaseFinosCoreServiceLegendCharm):
@@ -35,10 +50,26 @@ class LegendSdlcTestCase(legend_operator_testing.TestBaseFinosCoreServiceLegendC
         harness = ops_testing.Harness(LegendSdlcTestWrapper)
         return harness
 
-    @mock.patch("ops.testing._TestingPebbleClient.restart_services")
-    @mock.patch("ops.testing._TestingPebbleClient.start_services")
-    @mock.patch("ops.testing._TestingPebbleClient.stop_services")
-    def _test_relations_waiting(
-        self, _container_stop_mock, _container_start_mock, _container_restart_mock
-    ):
-        self._test_relations_waiting(_container_stop_mock, _container_restart_mock)
+    def test_get_core_legend_service_configs(self):
+        self._test_get_core_legend_service_configs()
+
+    def test_relations_waiting(self):
+        self._test_relations_waiting()
+
+    def test_studio_relation_joined(self):
+        self.harness.set_leader(True)
+        self.harness.begin_with_initial_hooks()
+
+        relator_name = "finos-legend-studio-k8s"
+        rel_id = self.harness.add_relation(charm.LEGEND_STUDIO_RELATION_NAME, relator_name)
+        relator_unit = "%s/0" % relator_name
+        self.harness.add_relation_unit(rel_id, relator_unit)
+        self.harness.update_relation_data(rel_id, relator_unit, {})
+
+        rel = self.harness.charm.framework.model.get_relation(
+            charm.LEGEND_STUDIO_RELATION_NAME, rel_id
+        )
+        self.assertEqual(
+            rel.data[self.harness.charm.app],
+            {"legend-sdlc-url": self.harness.charm._get_sdlc_service_url()},
+        )
